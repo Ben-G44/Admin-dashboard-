e254a07fdc4bf898015041b1d0bbfdfe1e998a6b
Bengitau <127228432+Ben-G44@users.noreply.github.com>
2025-08-13 20:11:17 +0000

    Pending changes exported from your codespace

--- models/Message.js
+++ models/Message.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const messageSchema = new mongoose.Schema({
   name: { type: String, required: true },
-    email: { type: String, required: true },
-      number: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+      number: { type: Number, required: true, unique: true },
+      password: {type: String, required: true },
         date: { type: Date, default: Date.now }
         });
 
+        messageSchema.plugin(uniqueValidator);
         module.exports = mongoose.model('Message', messageSchema);
         
